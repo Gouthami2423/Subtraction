99542e01056a385607a0d3ff6dd6923b48ec05cc
gouthami~ <kgowthami167@gmail.com>
2025-01-28 17:33:21 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,8 @@
+public class Subtraction {
+    public static void main(String[] args){
+        int num1 = 50;
+        int num2 = 30;
+        int result = num1 - num2;
+        System.out.println("the result of subtraction:"+ result);
+        }
+} 
